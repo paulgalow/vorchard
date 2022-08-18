e43f3c627446f748da3c88ac5b9dca590bf3cc18
Paul Galow <paul.galow@gmail.com>
2022-08-18 22:18:26 +0200

    Fix invalid author regex

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -220,7 +220,7 @@
                 id="author"
                 minlength="1"
                 maxlength="100"
-                pattern="^[\p&#123;L&#125;\d\._-!\?| ]+$"
+                pattern="^[\p&#123;L&#125;\d\-.?_!| ]+$"
                 inputmode="text"
                 value={author}
                 placeholder="Example Company"
