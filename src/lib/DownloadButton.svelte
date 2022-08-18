614ac5aabcbfdddc190765dc05cee5cd58d1f6f1
Paul Galow <paul.galow@gmail.com>
2022-08-18 22:12:50 +0200

    Move download button into component

---
+++ src/lib/DownloadButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<button
+  type="submit"
+  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5 mr-1"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+    />
+  </svg>
+  {title}
+</button>
