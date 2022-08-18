66c3439e6c69bb52cf8d3db66604d8e1eca5cfdf
Paul Galow <paul.galow@gmail.com>
2022-08-18 22:13:08 +0200

    Add reload button

---
+++ src/lib/ReloadButton.svelte
@@ -0,0 +1,21 @@
+<button
+  on:click={() => window.location.reload()}
+  type="button"
+  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-stone-400 hover:bg-stone-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+  aria-label="Reload page"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5 mr-1"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    stroke-width="2"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+    />
+  </svg>
+</button>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,8 @@
   import { createAuthName, createPassword } from "$lib/utils/strings";
   import Logo from "$lib/Logo.svelte";
   import Footer from "$lib/Footer.svelte";
+  import DownloadButton from "$lib/DownloadButton.svelte";
+  import ReloadButton from "$lib/ReloadButton.svelte";
   import renderTemplate from "$lib/templates/profile-eap-mschapv2";
 
   const SITE_NAME = "Vorchard";
@@ -54,8 +56,6 @@
     );
 
     downloadFile(configProfile, username);
-
-    //formEl.reset();
   }
   // async function onSubmit(e: SubmitEvent) {
   //   const formData = new FormData(<HTMLFormElement>e.target);
@@ -220,7 +220,7 @@
                 id="author"
                 minlength="1"
                 maxlength="100"
-                pattern="^[\p&#123;L&#125;\d[:punct:]| ]+$"
+                pattern="^[\p&#123;L&#125;\d\._-!\?| ]+$"
                 inputmode="text"
                 value={author}
                 placeholder="Example Company"
@@ -380,27 +380,11 @@
               </div>
             {/if}
           </div>
-          <div class="px-4 py-3 bg-orange-50 text-right sm:px-6">
-            <button
-              type="submit"
-              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5 mr-1"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                />
-              </svg>
-              Download profile
-            </button>
+          <div
+            class="flex items-center justify-end space-x-4 px-4 py-4 bg-orange-50 text-right sm:px-6"
+          >
+            <ReloadButton />
+            <DownloadButton title="Download profile" />
           </div>
         </div>
       </form>
