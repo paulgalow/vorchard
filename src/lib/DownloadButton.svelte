1bd10a6055d07a8bfb3182603a951e2dce7200dd
Paul Galow <paul.galow@gmail.com>
2022-08-19 21:49:18 +0200

    Reduce duplication of names

--- src/lib/DownloadButton.svelte
+++ src/lib/DownloadButton.svelte
@@ -5,7 +5,7 @@
 <button
   type="submit"
   class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
-  title="Download profile"
+  {title}
 >
   <svg
     xmlns="http://www.w3.org/2000/svg"
