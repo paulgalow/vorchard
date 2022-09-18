5376be44d8aaec9db1b2fe9e85102ac6ee2145df
Paul Galow <paul.galow@gmail.com>
2022-09-18 12:40:53 +0200

    Replace file upload icon

--- src/lib/UploadFileInput.svelte
+++ src/lib/UploadFileInput.svelte
@@ -15,19 +15,21 @@
   >
     <div class="space-y-1 text-center">
       <svg
-        class="mx-auto h-12 w-12 text-gray-400"
-        stroke="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        class="mx-auto h-9 w-9 mb-2 text-gray-400"
         fill="none"
-        viewBox="0 0 48 48"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
         aria-hidden="true"
       >
         <path
-          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-          stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
+          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
         />
       </svg>
+
       <div class="flex text-sm text-gray-600">
         <label
           for={name}
